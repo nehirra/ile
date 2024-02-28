252681dff2c4824f25f1a19fb1a1e2f900a5cecb
nehir <nehirrass@gmail.com>
2024-02-28 14:27:53 +0300

    coded figlet.py

--- figlet.py
+++ figlet.py
@@ -1,1 +1,30 @@
+import sys
+import random
+from pyfiglet import Figlet
+
+figlet = Figlet()
+font_liste =  figlet.getFonts()
+text = figlet
+
+if len(sys.argv) == 1:
+    text = input("Input: ")
+    chosen_font = random.choice(font_liste)
+    figlet.setFont(font=chosen_font)
+    text = figlet.renderText(text)
+elif len(sys.argv) == 3:
+    if sys.argv[1] in ("-f" , "--font"):
+        if sys.argv[2] not in font_liste:
+            print(f"Invalid font: {sys.argv[2]}")
+            sys.exit("Invalid usage")
+        else:
+            text = input("Input: ")
+            figlet.setFont(font=sys.argv[2])
+            text = figlet.renderText(text)
+    else:
+        sys.exit("Invalid usage")
+else:
+    sys.exit("Invalid usage")
+
+print("Output: \n"+text)
+sys.exit()
 
